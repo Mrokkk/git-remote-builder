557cdb48c6d8d57053b22938f5c530e3d184bffb
Mrokkk <mrokk@protonmail.com>
2017-01-16 21:50:35 +0100

    Improve messages

--- runner.sh
+++ runner.sh
@@ -4,14 +4,16 @@ name=$1
 branch=$2
 building_script=$3
 
+star="\e[1;35m*\e[0m"
+
 trap trigger SIGUSR1
 
 build_number=0
 
 trigger() {
     echo
-    echo "* $name build #$build_number @ `LANG=C date`"
-    echo "* Output is written also to file: $PWD/log.$build_number"
+    echo -e "$star $name build #$build_number @ `LANG=C date`"
+    echo -e "$star Output is written also to file: $PWD/log.$build_number"
     old_pwd=$PWD
     if [ ! -e $name ]; then
         git clone $name.git $name
@@ -22,9 +24,9 @@ trigger() {
     git submodule update --init --recursive
     unbuffer $building_script 2>&1 | tee $old_pwd/log.$build_number
     if [ "$?" == "0" ]; then
-        echo "* Built passed!"
+        echo -e "$star Build #$build_number \e[1;32mPASSED\e[0m"
     else
-        echo "* Built failed!"
+        echo -e "$star Build #$build_number \e[1;31mFAILED\e[0m"
     fi
     cd $old_pwd
     build_number=$((build_number+1))
