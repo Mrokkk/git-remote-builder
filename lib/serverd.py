295d8cdaafbfacc221691a5bf31dd79d3b341c43
Mrokkk <mrokk@protonmail.com>
2017-02-07 21:53:13 +0100

    WIP

--- lib/serverd.py
+++ lib/serverd.py
@@ -3,11 +3,30 @@
 import sys
 import os
 import time
+import socketserver
+import logging
 from daemons.prefab import run
+
+class Handler(socketserver.BaseRequestHandler):
+
+    def handle(self):
+        LOG = logging.getLogger('Handler.handle')
+        while True:
+            try:
+                self.data = self.request.recv(1024).strip()
+                if not self.data:
+                    return
+                LOG.debug('{} wrote: {}'.format(self.client_address[0], self.data.decode('ascii')))
+                self.request.sendto(b'OK\n', self.client_address)
+            except:
+                return
+
 
 class Serverd(run.RunDaemon):
 
     def run(self):
-        while True:
-            time.sleep(1)
-
+        host, port = 'localhost', 8090
+        LOG = logging.getLogger('Serverd.run')
+        with socketserver.TCPServer((host, port), Handler) as server:
+            LOG.info('Serving at port {}'.format(port))
+            server.serve_forever()
--- server.py
+++ server.py
@@ -7,19 +7,17 @@ import time
 from lib import serverd
 
 def main(argv):
-    action = sys.argv[1]
-    logfile = os.path.join(os.getcwd(), "serverd.log")
-    pidfile = os.path.join(os.getcwd(), "serverd.pid")
-
+    command = argv[0]
+    logfile = os.path.join(os.getcwd(), 'log')
+    pidfile = os.path.join(os.getcwd(), 'pid')
     logging.basicConfig(filename=logfile, level=logging.DEBUG)
-    d = serverd.Serverd(pidfile=pidfile)
-
-    if action == "start":
-        d.start()
-    elif action == "stop":
-        d.stop()
-    elif action == "restart":
-        d.restart()
+    server = serverd.Serverd(pidfile=pidfile)
+    if command == 'start':
+        server.start()
+    elif command == 'stop':
+        server.stop()
+    elif command == 'restart':
+        server.restart()
 
 
 if __name__ == '__main__':
