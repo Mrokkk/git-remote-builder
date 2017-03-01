30c4652f0cd7e9a11ea1579d984cad8f09df30cf
Mrokkk <mrokk@protonmail.com>
2017-03-01 21:10:30 +0100

    Don't print opened/closed connection information in LogProtocol

--- builderlib/log_reader.py
+++ builderlib/log_reader.py
@@ -28,11 +28,9 @@ class LogProtocol(asyncio.Protocol):
             self.on_open()
         self.file = open(self.log_name, 'w')
         self.peername = transport.get_extra_info('peername')
-        self.logger.info('{} opened connection'.format(self.peername))
         self.transport = transport
 
     def connection_lost(self, exc):
-        self.logger.info('{} closed connection'.format(self.peername))
         self.file.close()
         self.transport.close()
         if self.on_close:
