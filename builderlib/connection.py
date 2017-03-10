4e82168ce5b122db0abbb0948997bbb79c7beae3
Mrokkk <mrokk@protonmail.com>
2017-03-10 21:30:44 +0100

    Improvements

--- builderlib/connection.py
+++ builderlib/connection.py
@@ -1,6 +1,8 @@
 #!/bin/env python3
 
 import socket
+import asyncio
+import logging
 from .messages_pb2 import Result
 
 
@@ -14,6 +16,7 @@ class Connection:
             self.sock = ssl_context.wrap_socket(self.sock, server_side=False)
         self.sock.connect(address)
         self.sock.settimeout(10)
+        self.logger = logging.getLogger(self.__class__.__name__)
 
     def __del__(self):
         if self.sock:
@@ -37,5 +40,26 @@ class Connection:
             raise RuntimeError("Fail on server side!")
         return result
 
+    def error(self, error):
+        self.logger.error(error)
+        self.sock.close()
+
+    def dummy_read(self):
+        data = self.sock.recv(1024)
+        if not data:
+            return self.error('Cannot read result from server')
+        result = Result()
+        try:
+            result.ParseFromString(data)
+        except:
+            return self.error('Bad result message read from the server')
+        if result.code == Result.FAIL:
+            return self.error('Fail on server side!')
+
     def send_nowait(self, message):
         self.sock.sendall(message.SerializeToString())
+        try:
+            loop = asyncio.get_event_loop()
+        except:
+            loop = asyncio.new_event_loop()
+        loop.run_in_executor(None, self.dummy_read)
