346f2ec6783ee5ca520cad3d11d29bd96842abaf
Mrokkk <mrokk@protonmail.com>
2017-03-14 21:07:45 +0100

    slave_connection: refactoring

--- builderlib/master/slave_connection.py
+++ builderlib/master/slave_connection.py
@@ -1,22 +1,23 @@
 #!/bin env python3
 
 import logging
-from builderlib import messages_pb2
+from builderlib.messages_pb2 import *
 
 
 class SlaveConnection:
+
     connection = None
     token = None
     free = None
     address = None
 
-    def __init__(self, address, password, connection_factory):
+    def __init__(self, address, password, connection):
         self.logger = logging.getLogger("{}.{}".format(self.__class__.__name__, address))
         self.logger.debug('Constructor')
         self.address = address
         self.free = True
-        self.connection = connection_factory(address[0], address[1])
-        token_request = messages_pb2.SlaveCommand()
+        self.connection = connection
+        token_request = SlaveCommand()
         token_request.auth.password = password
         response = self.connection.send(token_request)
         if not response.token:
@@ -32,7 +33,7 @@ class SlaveConnection:
         self.free = False
 
     def send_build_request(self, repo_address, branch, log_server_port, script):
-        message = messages_pb2.SlaveCommand()
+        message = SlaveCommand()
         message.token = self.token
         message.build.repo_address = repo_address
         message.build.branch = branch
@@ -47,4 +48,5 @@ class SlaveConnectionFactory:
         self.connection_factory = connection_factory
 
     def create_connection(self, address, password):
-        return SlaveConnection(address, password, self.connection_factory)
+        return SlaveConnection(address, password,
+            self.connection_factory(address[0], address[1]))
